6967e8a578a37364d9aa05a2eaabd709419b2a30
kishiidddd <kishii_2022@qq.com>
2025-12-13 11:33:10 +0800

    Initial commit: 项目创建

---
+++ ItemManage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItemManage
+//
+//  Created by xiny on 2025/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
